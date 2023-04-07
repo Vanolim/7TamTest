f4539436b120d25a7ecc251000d647b51f5f35c3
Vanolim <legavana@gmail.com>
2023-04-07 19:43:27 +0300

    Update

--- Assets/Scripts/Game/Core/Infrastructure/GameInstaller.cs
+++ Assets/Scripts/Game/Core/Infrastructure/GameInstaller.cs
@@ -15,6 +15,8 @@ namespace TapTest
         
         public override void InstallBindings()
         {
+            PhotonNetwork.AutomaticallySyncScene = true;
+            
             BindPrefab(_gameProvider.Character);
             BindPrefab(_gameProvider.GamePhotonService);
             BindPrefab(_gameProvider.GameBoard);
